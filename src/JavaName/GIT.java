fee2cce745a08ed8c851b893bfe800c50af07204
Denis <some@email>
2020-10-07 09:26:17 +0200

    10 skupinaB

--- src/JavaName/GIT.java
+++ src/JavaName/GIT.java
@@ -15,10 +15,10 @@ public class GIT {
         Scanner scanner = new Scanner(System.in);
 
         String out = scanner.nextLine();
-        int pocet = pocetZnakov("out");
+        int pocet = pocetZnakov(out);
         //vytvorte metodu ktora počita počet znakov v zadanom texte
         System.out.println("vystupny text:"+out);
-        System.out.println("pocet znakov je: "+ 5);
+        System.out.println("pocet znakov je: "+ pocet);
         InputStream inputStream; //binarny subor
         InputStreamReader reader; // znakovy subor nejaky text
 
@@ -26,8 +26,8 @@ public class GIT {
     }
     //komentar na GIT
     //vstupna metoda bude String a vystup bude int
-    private static int pocetZnakov(String out) {
-        int langht: wordcount /;
-        return count;
+    private static int pocetZnakov(String vstup) {
+        return vstup.length();
+
     }
 }
